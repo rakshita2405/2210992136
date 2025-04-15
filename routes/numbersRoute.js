f5c6844e8a3cd71de173e7a99513ce0a08080c9d
Rakshita Bhardwaj <122474971+rakshita2405@users.noreply.github.com>
2025-04-15 13:33:57 +0530

    Delete routes directory

--- routes/numbersRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getNumbers } = require('../controllers/numbersController');
-
-router.get('/numbers/:numberid', getNumbers);
-
-module.exports = router;
